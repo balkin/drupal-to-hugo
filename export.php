d969d5a33228b61bec35a37c36c67c65ca3b42a8
Ruslan Balkin <baron@baron.su>
2016-05-25 01:47:06 +0300

    OOP implementation

--- export.php
+++ export.php
@@ -13,36 +13,65 @@ module_load_include('module', 'field');
 $GLOBALS['base_path'] = '';
 
 error_reporting(E_ALL);
-$categoriesList = taxonomy_get_tree(2);
-$tagsList = taxonomy_get_tree(4);
 
-printf("Loaded %d categories and %d tags\n", count(categoriesList), count(tagsList));
-foreach ($categoriesList as $c) {
-	$categories[$c->tid] = $c;
-}
-foreach ($tagsList as $c) {
-	$tags[$c->tid] = $c;
+class DrupalTaxonomy {
+	private $categories = array();
+	private $tags = array();
+
+	function __construct($category_tid, $tags_tid) {
+		$categoriesList = taxonomy_get_tree($category_tid);
+		$tagsList = taxonomy_get_tree($tags_tid);
+		foreach ($categoriesList as $c) {
+			$this->categories[$c->tid] = $c;
+		}
+		foreach ($tagsList as $c) {
+			$this->tags[$c->tid] = $c;
+		}
+	}
+
+	public function categoriesCount() { return count($this->categories); }
+	public function tagsCount() { return count($this->tags); }
+
+	public function getCategoryMapper() {
+		return function($x) { return $this->categories[$x['tid']]; };
+	}
+	public function getTagMapper() {
+		return function($x) { return $this->tags[$x['tid']]; };
+	}
 }
 
 $taxonomyExtractor = function($what) { return $what->name; };
 
+class HugoPage {
+	private $url;
+	private $node;
+	private $tags;
+	private $topics;
+	function __construct($node, $tags, $topics) {
+		$this->node = $node;
+		$this->url = url(drupal_get_path_alias('node/' . $node->nid));
+		$this->tags = array_unique($tags);
+		$this->topics = array_unique($topics);
+	}
+	function __toString() {
+		return sprintf("%5d. %s %s\n       T: %s\n       C: %s\n", $this->node->nid, $this->node->title, $this->url, 
+					implode(', ', $this->tags), implode(', ', $this->topics));
+	}
+}
+
+$drupalTaxonomy = new DrupalTaxonomy(2, 4);
+printf("Loaded %d categories and %d tags\n", $drupalTaxonomy->categoriesCount(), $drupalTaxonomy->tagsCount());
 $result = db_query("SELECT nid FROM {node}");
 foreach ($result as $tmp) {
 	$node = node_load($tmp->nid);
-	$url = url(drupal_get_path_alias('node/' . $node->nid));
 	$hugoTopics = array(); 
 	$hugoTags = array();
 	foreach ($node->field_category as $language => $languageCategories) {
-		foreach ($languageCategories as $category) {
-			array_push($hugoTopics, $categories[$category['tid']]);
-		}
+		$hugoTopics += array_map($drupalTaxonomy->getCategoryMapper(), $languageCategories);
 	}
 	foreach ($node->field_tags as $language => $languageTags) {
-		foreach ($languageTags as $tag) {
-			array_push($hugoTags, $tags[$tag['tid']]);
-		}
+		$hugoTags += array_map($drupalTaxonomy->getTagMapper(), $languageTags);
 	}
-	$finalTags = array_unique(array_map($taxonomyExtractor, $hugoTags));
-	$finalTopics = array_unique(array_map($taxonomyExtractor, $hugoTopics));
-	printf("%5d. %s %s\n       T: %s\n       C: %s\n", $tmp->nid, $node->title, $url, implode(', ', $finalTags), implode(', ', $finalTopics));
+	$page = new HugoPage($node, array_map($taxonomyExtractor, $hugoTags), array_map($taxonomyExtractor, $hugoTopics));
+	echo $page;
 }
